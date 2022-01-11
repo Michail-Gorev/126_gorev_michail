87ca2fe249e6ca78380d061b06a9736581c768bb
Michail-Gorev <90262826+Michail-Gorev@users.noreply.github.com>
2022-01-11 20:06:18 +0300

    3

--- Contract_manager/src/comp/company/ContractTests.java
+++ Contract_manager/src/comp/company/ContractTests.java
@@ -41,4 +41,14 @@ public class ContractTests extends Assert {
         contractManager.registerPaymentDocument(1000,901,PaymentOrder,"1","20200101");
         assertEquals(1, contractManager.getContracts().get("1").getDocumentCount());
     }
+    @Test
+    public void registerPaymentDocument_RegisterPaymentDocumentWithData_DocumentsCountEqualsFour(){
+        manager contractManager = manager.create();
+        contractManager.addContract("1","20211218");
+        contractManager.registerPaymentDocument(1000,1,PaymentOrder,"1","20200101");
+        contractManager.registerPaymentDocument(200,12,BankOrder,"1","20200101");
+        contractManager.registerPaymentDocument(500,3,PaymentOrder,"1","20200101");
+        contractManager.registerPaymentDocument(100,4,BankOrder,"1","20200101");
+        assertEquals(4, contractManager.getContracts().get("1").getDocumentCount());
+    }
 }
