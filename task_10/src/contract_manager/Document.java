d22d60ee87c8017e2efca89c6b5def8d10131f59
Michail-Gorev <90262826+Michail-Gorev@users.noreply.github.com>
2022-01-16 00:29:11 +0300

    complete test

--- task_10/src/contract_manager/Document.java
+++ task_10/src/contract_manager/Document.java
@@ -5,15 +5,23 @@ public class Document {
     private String date;
 
     private HashMap<Integer, PaymentDocument> paymentDocuments;
-
+    private int sumOfDocuments;
     public Document(String date){
         this.date = date;
         paymentDocuments = new HashMap<>();
+        sumOfDocuments = 0;
     }
     public int getDocumentsNumber(){
         return paymentDocuments.size();
     }
     public void registerDocument(int sum, int number, DocTypes docTypes, String date){
         paymentDocuments.put(number, new PaymentDocument(sum,  docTypes, date));
+        sumOfDocuments += sum;
+    }
+    public int getSumOfDocuments(){
+        return sumOfDocuments;
+    }
+    public HashMap<Integer, PaymentDocument> getPaymentDocuments(){
+        return paymentDocuments;
     }
 }
--- task_10/src/contract_manager/Manager.java
+++ task_10/src/contract_manager/Manager.java
@@ -1,6 +1,9 @@
 package contract_manager;
 
 import java.util.HashMap;
+
+import java.util.ArrayList;
+import java.util.List;
 public class Manager {
     private HashMap<String, Document> documents;
 
@@ -21,5 +24,14 @@ public class Manager {
     }
     public void registerDocument(int sum, int documentNumber, DocTypes docTypes, String documentNum, String date) {
         documents.get(documentNum).registerDocument(sum,documentNumber,docTypes,date);
+    }
+    public static List<Integer> getAllDocuments() {
+        List<Integer>  DOCUMENTS = new ArrayList();
+        for(Document contract : documents.values()){
+            for(PaymentDocument paymentDoc : contract.getPaymentDocuments().values())
+                DOCUMENTS.add(paymentDoc.getSum());
+        }
+
+        return DOCUMENTS;
     }
 }
