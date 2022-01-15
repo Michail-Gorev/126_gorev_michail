ed1c3370311b705cc4001ca61f805a4b23cfba22
Michail-Gorev <90262826+Michail-Gorev@users.noreply.github.com>
2022-01-15 22:29:52 +0300

    add addDocument_AddDocumentWithNumberAndDate_DocumentsNumberEqualsOne test

--- task_10/src/contract_manager_tests/ContractManagerTests.java
+++ task_10/src/contract_manager_tests/ContractManagerTests.java
@@ -8,6 +8,12 @@ public class ContractManagerTests extends Assert {
     @Test
     public void create_CreateListOfDocuments_DocumentsNumberEqualsZero() {
         Documents document = Documents.create();
-        assertEquals(0,document.getDocumentNumber());
+        assertEquals(0,document.getDocumentsNumber());
+    }
+    @Test
+    public void addDocument_AddDocumentWithNumberAndDate_DocumentsNumberEqualsOne() {
+        Documents document = Documents.create();
+        document.addDocument("1", "20200101");
+        assertEquals(1, document.getDocumentsNumber);
     }
 }
