e25f109af73e03daeca866897fa57437ce52ccc0
Michail-Gorev <90262826+Michail-Gorev@users.noreply.github.com>
2022-04-07 20:02:56 +0300

    fixing

--- 2sem/hw/task_2/files/Task2_Tests.java
+++ 2sem/hw/task_2/files/Task2_Tests.java
@@ -1,92 +1,84 @@
 import org.junit.*;
 
 public class Task2_Tests extends Assert{
-    class TComparator implements MyComparator<Integer>{
+    class TestComparator implements MyComparator<Integer>{
         @Override
         public int compare(Integer first, Integer second) {
             return first - second;
         }
     }
+
     @Test
-    public void bubbleSort_ArrayContainsOnlySameNumbers_ArrayNotChanges(){
+    public void bubbleSort_ArrayContainsOnlySameNumbers_NoChanges(){
         try {
             BubbleSort<Integer> bubbleSort = new BubbleSort<>();
-            Integer[] arr = {0, 0, 0, 0, 0};
-
-            bubbleSort.sort(arr, new TComparator());
-
-            assertArrayEquals(new Integer[]{0, 0, 0, 0, 0}, arr);
+            Integer[] arr = {0, 0, 0};
+            bubbleSort.sort(arr, new TestComparator());
+            assertArrayEquals(new Integer[]{0, 0, 0}, arr);
         }
         catch (Exception e){
             fail();
         }
     }
+
     @Test
-    public void bubbleSort_InvertedArrayWithRightBiggestItem_ArrayIsSorted(){
+    public void bubbleSort_InvertedArrayWithTheSameLastBiggestItem_ArrayIsSorted(){
         BubbleSort<Integer> bubbleSort = new BubbleSort<>();
-        Integer[] arr = {10, 15, 89, 0, 98};
-
-        bubbleSort.sort(arr, new TComparator());
-
-        assertArrayEquals(new Integer[]{0, 10, 15, 89, 98}, arr);
+        Integer[] arr = {3, 2, 1, 4};
+        bubbleSort.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{1, 2, 3, 4}, arr);
     }
+
     @Test
     public void bubbleSort_InvertedArray_ArrayIsSorted(){
         BubbleSort<Integer> bubbleSort = new BubbleSort<>();
-        Integer[] arr = {10, 5, 1};
-
-        bubbleSort.sort(arr, new TComparator());
-
-        assertArrayEquals(new Integer[]{1, 5, 10}, arr);
+        Integer[] arr = {3, 2, 1};
+        bubbleSort.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{1, 2, 3}, arr);
     }
+
     @Test
     public void bubbleSort_UnsortedArray_ArrayIsSorted(){
         BubbleSort<Integer> bubbleSort = new BubbleSort<>();
-        Integer[] arr = {5,3,4,1};
-
-        bubbleSort.sort(arr, new TComparator());
-
-        assertArrayEquals(new Integer[]{1, 3, 4, 5}, arr);
+        Integer[] arr = {15,23,5,0};
+        bubbleSort.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{0, 5, 15, 23}, arr);
     }
+
     @Test
-    public void selectionSort_InvertedArrayThreeItems_ArrayIsSorted(){
+    public void selectionSort_InvertedArrayLengthTwo_ArrayIsSorted(){
         SelectionSort<Integer> selectionSort = new SelectionSort<>();
-        Integer[] arr = {5, 4, 3};
-
-        selectionSort.sort(arr, new TComparator());
-
-        assertArrayEquals(new Integer[]{3, 4, 5}, arr);
+        Integer[] arr = {1, 0};
+        selectionSort.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{0, 1}, arr);
     }
+
     @Test
     public void selectionSort_UnsortedArray_ArrayIsSorted(){
         SelectionSort<Integer> selectionSort = new SelectionSort<>();
-        Integer[] arr = {5, 4, 10, 1};
-
-        selectionSort.sort(arr, new TComparator());
-
-        assertArrayEquals(new Integer[]{1, 4, 5, 10}, arr);
+        Integer[] arr = {1, 0, 5, 34};
+        selectionSort.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{0, 1, 5, 34}, arr);
     }
+
     @Test
-    public void insertionSort_ArrayContainsOnlySameNumbers_ArrayNotChanges(){
+    public void insertionSort_ArrayContainsOnlySameNumbers_NoChanges(){
         try {
             InsertionSort<Integer> insertionSort = new InsertionSort<>();
-            Integer[] arr = {0, 0, 0, 0};
-
-            insertionSort.sort(arr, new TComparator());
-
-            assertArrayEquals(new Integer[]{0, 0, 0, 0}, arr);
+            Integer[] arr = {10, 10};
+            insertionSort.sort(arr, new TestComparator());
+            assertArrayEquals(new Integer[]{10, 10}, arr);
         }
         catch (Exception e){
             fail();
         }
     }
+
     @Test
-    public void insertionSort_SortedArray_ArrayHasNotChanged(){
+    public void insertionSort_SortedArray_NoChanges(){
         InsertionSort<Integer> insertionSort = new InsertionSort<>();
-        Integer[] arr = {0, 3, 5, 10};
-
-        insertionSort.sort(arr, new TComparator());
-
-        assertArrayEquals(new Integer[]{0, 3, 5, 10}, arr);
+        Integer[] arr = {1,2};
+        insertionSort.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{1,2}, arr);
     }
 }
